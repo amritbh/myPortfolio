be39ff3d8683e54a8afa7231653a13f98e33046d
Amrit Bhattarai <bhattarai.amrit90@gmail.com>
2026-07-04 15:22:47 -0700

    style(infra): fix terraform formatting

--- infra/modules/backend/main.tf
+++ infra/modules/backend/main.tf
@@ -3,7 +3,7 @@ resource "aws_dynamodb_table" "blogs_table" {
   name         = "${var.project_name}-${var.environment}-blogs"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "slug"
-  
+
   deletion_protection_enabled = true
 
   attribute {
