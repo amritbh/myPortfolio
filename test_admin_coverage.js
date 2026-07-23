26be4a34097bfe55f27df09969e79d2dcea16233
Amrit Bhattarai <bhattarai.amrit90@gmail.com>
2026-07-22 21:32:57 -0700

    chore: remove accidental test_admin_coverage.js file from root directory

--- test_admin_coverage.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import AdminDashboard from "./src/pages/admin/AdminDashboard";
-import { MemoryRouter, Route } from "react-router-dom";
-import * as apiClient from "./src/utils/apiClient";
-// ...
